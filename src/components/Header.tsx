8bd81a19bb1258610f48b589e3be2b389047b294
Himanshu Kataria <katariahk03@gmail.com>
2022-07-24 12:58:34 +0530

    Header add

---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+import "./Header.scss";
+export const Header = () => {
+  return (
+    <header className="header x">
+      <span className="header__brand">
+        <span className="header__brand--first">Bad </span>
+        <span className="header__brand--second">Experience</span>
+      </span>
+    </header>
+  );
+};
